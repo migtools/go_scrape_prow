c140f2680fca652443c0c2b9294866e66b412dcc
Wes Hayutin <whayutin@redhat.com>
2022-04-14 09:42:30 -0400

    entrypoint script was causing grafana to restart
    * every 10 seconds the grafana sidecare was reloading
    * this made it impossible to save any changes to the UI
    * the issue is w/ the key creation and import json mounts and urls
      * will fix in a later patch
    
    * for now... key creating and importing json should be done on
    the node and point to localhost:8080 vs. grafana:3000

--- grafana/entrypoint.sh
+++ grafana/entrypoint.sh
@@ -11,15 +11,16 @@ api_url=http://$admin_user:$admin_password@$host
 # Wait a little for grafana to startup
 sleep 10
 
-if [ ! -f $api_key_path ]; then
-    if ! ./create-api-key.py --url $api_url --key-name ${api_key_name} > $api_key_path; then
-        rm $api_key_path
-    fi
-fi
-
-
-./import-grafana.py --host http://$host --key $api_key_path
+#if [ ! -f $api_key_path ]; then
+#    if ! ./create-api-key.py --url $api_url --key-name ${api_key_name} > $api_key_path; then
+#        rm $api_key_path
+#    fi
+#fi
+#
+#
+#./import-grafana.py --host http://$host --key $api_key_path
 
 #./grafana-alert-notification-server.py \
 #                        --grafana-host http://$host \
 #                        --grafana-key $(cat $api_key_path)
+tail -f /dev/null
